8d0f67defde62501bf439b2a983da9207d45c986
Daniel Smółka <smowling@gmail.com>
2024-01-12 13:48:36 +0100

    more setup

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # bikecheck
 
-This template should help get you started developing with Vue 3 in Vite.
+Bikecheck frontend with vue + supabase
 
 ## Recommended IDE Setup
 
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,24 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<script setup>
+  import { ref, onMounted } from 'vue'
+  import { supabase } from '../lib/supabaseClient'
+
+  const countries = ref([])
+
+  async function getTests() {
+    const { data } = await supabase.from('custom').select()
+    tests.value = data
+  }
+
+  onMounted(() => {
+    getTests()
+  })
+  </script>
+
+  <template>
+    <ul>
+      <li v-for="test in tests" :key="test.id">{{ test.details }}</li>
+    </ul>
+  </template>
 
 <style>
 @media (min-width: 1024px) {
