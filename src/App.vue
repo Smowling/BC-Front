f95279975be6363dbf92bf045816c426719c65a3
Daniel Smółka <smowling@gmail.com>
2024-01-16 21:28:43 +0100

    learning

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@ onMounted(() => {
     <li v-for="cus in custdata" :key="cus.id">{{ cus.details }}</li>
   </ul>
 </template> -->
-
+<!-- 
 <script setup>
 import { onMounted, ref } from 'vue'
 import Account from './components/Account.vue'
@@ -48,4 +48,50 @@ onMounted(() => {
     <Account v-if="session" :session="session" />
     <Auth v-else />
   </div>
+</template> -->
+
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/secret">Secret</RouterLink>
+		<RouterLink to="/login">Login</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <div class="content">
+	<RouterView />
+  </div>
+
 </template>
+
+<style scoped>
+nav {
+	display: flex;
+	justify-content: center;
+}
+
+nav a {
+	padding: 1em;
+}
+
+.content {
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	width: 500px;
+	border: solid 1px black;
+	margin-left: auto;
+	margin-right: auto;
+	border-radius: 1em;
+	padding: 2em 0 2em 0;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,60 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import UnauthorizedView from '../views/UnauthorizedView.vue'
+import SecretView from '../views/SecretView.vue'
+
+import { supabase } from '../lib/supabaseClient'
+
+let localUser;
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-
-
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/secret',
+      name: 'secret',
+      component: SecretView,
+	  meta: { requiresAuth: true }
+    },
+	{
+      path: '/login',
+	  name: 'login',
+	  component: LoginView
+	},
+	{
+	  path: '/unauthorized',
+	  name: 'unauthorized',
+	  component: UnauthorizedView
+	}
   ]
 })
+
+async function getUser(next) {
+	localUser = await supabase.auth.getSession();
+	if (localUser.data.session == null) {
+		next('/unauthorized')
+	}
+	else {
+		next();
+	}
+}
+
+
+router.beforeEach((to, from, next) => {
+	if (to.meta.requiresAuth) {
+		getUser(next);
+	}
+	else {
+		next();
+	}
+})
+
+
 
 export default router
